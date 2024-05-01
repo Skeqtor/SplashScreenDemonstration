2ccc11e8848265b966e7eb9499a036712035a4d5
- <->
2024-05-02 01:36:20 +0700

    Add project files.

---
+++ SplashScreenDemonstration/frmMain.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SplashScreenDemonstration
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            Thread t = new Thread(new ThreadStart(StartForm));
+            t.Start();
+            Thread.Sleep(10000);
+            InitializeComponent();
+            t.Abort();
+        }
+
+        public void StartForm()
+        {
+            Application.Run(new frmSplashScreenDemonstration());
+        }
+    }
+}
